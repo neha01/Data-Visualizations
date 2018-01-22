2acf8fa4ee1b364918c43f69e9289fc02fd0b140
neha01 <nehayadav0102@gmail.com>
2018-01-22 14:19:29 +0530

    Update flight_route - practice.R

--- Flight_route_visualization_in_R/flight_route - practice.R
+++ Flight_route_visualization_in_R/flight_route - practice.R
@@ -5,67 +5,45 @@ library(maptools)
 library(ggmap)
 library(geosphere)
 library(plyr)
-gpclibPermit()
 
-setwd('F:\\2017-SOFTWARE-WORKSPACES\\FLIGHT-DATA-PROJECT')
-rm(list = ls())
 
-fortify.SpatialLinesDataFrame = function(model, data, ...){
-  ldply(model@lines, fortify)
-}
 
-df = read.csv('routes_all.csv', stringsAsFactor=F)
-plot_countries = c('United States', 'United Kingdom', 'Spain', 'Germany', 'France', 'Italy','China', 'Singapore')
-df = df[df$from %in% plot_countries,]
+df = read.csv('F:/2017-SOFTWARE-WORKSPACES/FLIGHT-DATA-PROJECT/routes_all.csv',stringsAsFactor=F)
+df=df[df$from %in% 'India',]
+#print(df)
 
+# calculating rotes
 routes = gcIntermediate(df[,c('lon1', 'lat1')], df[,c('lon2', 'lat2')], 200, breakAtDateLine = FALSE, addStartEnd = TRUE, sp=TRUE)
 
-fortifiedroutes = fortify.SpatialLinesDataFrame(routes) 
 
-#### merge to form great circles 
-routes_count = data.frame('count'=df$count, 'id'=1:nrow(df), 'Countries'=df$from)
-greatcircles = merge(fortifiedroutes, routes_count, all.x=T, by='id')
+fortify_routes=ldply(routes@lines, fortify)
 
-####
+# merge to form great circles
+num_routes = data.frame('count'=df$count, 'id'=1:nrow(df), 'Countries'=df$from)
+greatcircles = merge(fortify_routes, num_routes, all.x=T, by='id')
+
 
 worldmap = map_data ("world")
 
-wrld<-c(geom_polygon(aes(long,lat,group=group), size = 0.1, colour= "#090D2A", 
-                     fill="#090D2A", alpha=0.8, data=worldmap))
-
-urbanareasin <- readShapePoly("ne_10m_urban_areas.shp")
+# background layer
+background<-c(geom_polygon(aes(long,lat,group=group), size = 0.1, colour= "#000000",
+                     fill="#000000", alpha=0.8, data=worldmap))
+# urban layer
+urbanareas <- readShapePoly("F:/2017-SOFTWARE-WORKSPACES/15 JAN 2018 FLIGHT DATA PRACTICE/ne_10m_urban_areas.shp")
 urb <- c(geom_polygon(aes(long, lat, group = group),
-                    size = 0.3,
-                    color = "#008000",
-                    fill = "#ffffff",
-                    alpha = 0.8,
-                    data = urbanareasin))
-####jpeg(filename="nehaprojectlatest.jpeg")
-####tiff(filename = "nehaproject",res = 800, pointsize = 2)
-png("flightdataproject.png", width = 2500, height = 1200)
+                      size = 0.3,
+                      color = "#ffffff",
+                      fill = "#ffffff",
+                      alpha = 0.6,
+                      data = urbanareas))
 
-plot(ggplot() + 
-wrld +
-urb + 
-geom_line(aes(long,lat,group=id, color=Countries), alpha = 0.3, size=0.01, data= greatcircles) + 
-theme(panel.background = element_rect(fill='#00001C',colour='#00001C'), panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-annotate("text",x=max(worldmap$long),y=-60,hjust=.9,size=6,
-        label=paste("Flight routes from top 8 countries","Data From OpenFlights.org", sep="\n"),color="white"))
-
-dev.off
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# combine
+ggplot() +
+  background +
+  urb +
+  geom_line(aes(long,lat,group=id, color=Countries), alpha = 0.3, size=0.01, data= greatcircles) +scale_color_manual(values=c("#0000ff"))+
+  theme(panel.background = element_rect(fill='#00001C',colour='#00001C'), panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  
+   
+  annotate("text",x=max(worldmap$long),y=-60,hjust=.9,size=6,
+           label=paste("International Flight Routes via INDIA","Data From OpenFlights.org", sep="\n"),color="white")
